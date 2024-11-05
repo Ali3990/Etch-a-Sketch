f7c3d4170b362a0c964cb81df7620835d5ce416a
Alan <61648323+Ali3990@users.noreply.github.com>
2024-11-04 22:07:56 -0800

    version 0.1

---
+++ EtchSketch.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container")
+
+for(let i = 0; i < 16; i++){
+    const div = document.createElement("div");
+    div.textContent = `Div ${i+1}`;
+
+    div.style.backgroundColor = 'lightblue';
+    div.style.margin = '5px';
+    div.style.padding = '5px';
+    // div.style.textAlign = 'center'
+    // div.style.display = ''
+
+    container.appendChild(div)
+}
